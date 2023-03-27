f1d1dcd3e65a2bdda8d1afaad431aea5c54f612d
Rihanul Islam <rihanthedev@gmail.com>
2023-03-28 01:25:40 +0600

    cart order summary

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,11 +3,27 @@ import './Cart.css';
 
 const Cart = (props) => {
     console.log(props);
+    const {cart} = props;
+    console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    
+    for(const product of cart){
+        total = total+product.price;
+        shipping = shipping+ product.shipping;
+        
+    }
+    let tax = total * 7/100;
+    console.log(tax);
+    const GrandTotal = total + tax+ shipping;
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Items: {props.cart.length}</p>
-            <p>Total Price:</p>
+            <p>Selected Items: {cart.length}</p>
+            <p>Total Price: $ {total}</p>
+            <p>Total Shiping:$ {shipping}  </p>
+            <p>Tax: $ {tax.toFixed(2)} </p>
+            <h6>Grand Total:${GrandTotal.toFixed(2)}</h6>
         </div>
     );
 };
