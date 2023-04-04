adf0e134aed007bbe63067206d888a9becc57bf3
Rihanul Islam <rihanthedev@gmail.com>
2023-04-04 22:29:06 +0600

    Login Page added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import './index.css'
+import Home from './components/Home/Home';
+import Shop from './components/Shop/Shop';
+import Login from './components/Login/Login';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home></Home>,
+    children:[
+      {
+        path:'/',
+        element: <Shop></Shop>
+      },
+      {
+        path:'login',
+        element: <Login></Login>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
