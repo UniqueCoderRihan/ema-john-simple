9a080b8b5f9e97b2306b0e843e869760e1605590
Rihanul Islam <rihanthedev@gmail.com>
2023-04-04 22:59:47 +0600

    Cart

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Cart from '../Cart/Cart';
+import '../Shop/Shop.css'
 
 const Orders = () => {
     return (
-        <div>
-            <h3>This is Order Page</h3>
+        <div className='shop-container'>
+            <div className='products-container'>
+                <h3>This is Order Page</h3>
+            </div>
+            <div className='cart-container'>
+                <Cart cart={[]}></Cart>
+            </div>
         </div>
     );
 };
