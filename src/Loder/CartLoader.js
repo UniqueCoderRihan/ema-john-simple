31ea0f32fea88530f868dda8509ccdbad339e8ee
Rihanul Islam <rihanthedev@gmail.com>
2023-04-05 19:58:07 +0600

    localStorge data use of multiple router

--- src/Loder/CartLoader.js
+++ src/Loder/CartLoader.js
@@ -1,6 +1,24 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
 const CartProductsLoader = async ()=>{
     const loadedProducts = await fetch('products.json')
     const products = await loadedProducts.json();
-    console.log(products);
+
+    // If cart in your database, you have to use asynce
+    // storeCart ta hocce  local store ta re manage korar ekta variable. 
+    // amra for of marbo array er upor ar for in marbo object er upor.
+    const storeCart = getShoppingCart();
+    const savedCart = [];
+    for(const id in storeCart){
+        const adddedProduct = products.find(pd => pd.id === id);
+        if(adddedProduct){
+            const quantity = storeCart[id];
+            adddedProduct.quantity = quantity;
+            savedCart.push(adddedProduct);
+        }
+    }
+    console.log(savedCart);
+    return savedCart;
+
 }
 export default CartProductsLoader
--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Cart from '../Cart/Cart';
 import '../Shop/Shop.css'
+import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
+
+    const cart = useLoaderData();
+    console.log(cart);
+
     return (
         <div className='shop-container'>
             <div className='products-container'>
-                <h3>This is Order Page</h3>
+                <h3>This is Order Page{cart.length} </h3>
             </div>
             <div className='cart-container'>
-                <Cart cart={[]}></Cart>
+                <Cart cart={cart} ></Cart>
             </div>
         </div>
     );
