c32c440ddda1d1704f0bcb5225373272f7758208
Rihanul Islam <rihanthedev@gmail.com>
2023-04-18 05:39:46 +0600

    login added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,8 +3,19 @@ import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            <h3>Login Comming Soon</h3>
+        <div className='form-container'>
+            <h3 className='form-title'>Login</h3>
+            <form>
+                <div className='form-control'>
+                    <label htmlFor="">Email</label>
+                    <input type="email" name='email' id='email' required />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor="">Password</label>
+                    <input type="password" name='password' id='password' required />
+                </div>
+                <button className='btn-submit'>Login</button>
+            </form>
         </div>
     );
 };
